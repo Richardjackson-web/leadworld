b3fa0f10b9145912a738e2e35489b24593cf025c
Richardjackson-web <richardjackson556@gmail.com>
2022-09-03 04:21:09 +0100

    Still making changes

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './Hero.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Carousel from 'react-bootstrap/Carousel';
-// import Img1 from '../../Asset/Images/hero1.jpg';
-// import Img2 from '../../Asset/Images/Hero2.jpg';
-// import Img3 from '../../Asset/Images/Hero3.jpg';
 
 export default function Hero() {
   return (
-    <div>
-
+    <div className="background-hero">
         <div className="hero">.
             <h1>
                 Raising <br /> a generation of young persons <br className="break" /> with
@@ -19,43 +13,6 @@ export default function Hero() {
             <button>Become a leader</button>
             </div>
         </div>
-         {/* <Carousel>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100 h-50"
-          src={Img1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100 h-50"
-          src={Img2}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 h-50"
-          src={Img3}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel> */}
     </div>
   )
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './Home.css';
+import Forms from '../../Components/Forms/Forms';
 import Header from '../../Components/Header/Header';
 import Hero from '../../Components/Hero/Hero';
 
 export default function Home() {
   return (
-    <div>
+    <div className="home-background">
         <Header />
         <Hero />
+        <Forms />
     </div>
   )
 }
