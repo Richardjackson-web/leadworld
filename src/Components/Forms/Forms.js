86be3c75ad820328520d84f848c0f536bcea5ba9
Richardjackson-web <richardjackson556@gmail.com>
2022-09-03 04:52:31 +0100

    Still making changes

--- src/Components/Forms/Forms.js
+++ src/Components/Forms/Forms.js
@@ -1,30 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 
 export default function Forms() {
+    const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <div className="container back">
         <h1>Register for LPC Conference</h1>
-        <Row className="g-2 form-row">
-      <Col md>
-        <FloatingLabel controlId="floatingInputGrid" label="Name">
-          <Form.Control type="email" placeholder="name@example.com" />
-        </FloatingLabel><br />
-        <FloatingLabel controlId="floatingInputGrid" label="Email address">
-          <Form.Control type="email" placeholder="name@example.com" />
-        </FloatingLabel><br />
-        <FloatingLabel controlId="floatingInputGrid" label="Phone Number">
-          <Form.Control type="email" placeholder="name@example.com" />
-        </FloatingLabel><br />
-        <FloatingLabel controlId="floatingInputGrid" label="Email address">
-          <Form.Control type="email" placeholder="name@example.com" />
-        </FloatingLabel>
-      </Col>
-    </Row>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+          <Form.Label>Email Address</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+            <Form.Control
+              type="text"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustom03">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="text" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom04">
+          <Form.Label>Expectations from the conference</Form.Label>
+          <Form.Control type="text" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid state.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Button type="submit">Submit form</Button>
+    </Form>
     </div>
   )
 }
