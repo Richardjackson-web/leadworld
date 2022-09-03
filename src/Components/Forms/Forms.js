a3049451d7ebfd3fbf56f6c06f0b13cab81b72bf
Richardjackson-web <richardjackson556@gmail.com>
2022-09-03 04:32:02 +0100

    Still making changes

--- src/Components/Forms/Forms.js
+++ src/Components/Forms/Forms.js
@@ -9,23 +9,19 @@ export default function Forms() {
   return (
     <div className="container back">
         <h1>Register for LPC Conference</h1>
-        <Row className="g-2">
+        <Row className="g-2 form-row">
       <Col md>
+        <FloatingLabel controlId="floatingInputGrid" label="Name">
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel><br />
         <FloatingLabel controlId="floatingInputGrid" label="Email address">
           <Form.Control type="email" placeholder="name@example.com" />
-        </FloatingLabel>
-      </Col>
-      <Col md>
-        <FloatingLabel
-          controlId="floatingSelectGrid"
-          label="Works with selects"
-        >
-          <Form.Select aria-label="Floating label select example">
-            <option>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </Form.Select>
+        </FloatingLabel><br />
+        <FloatingLabel controlId="floatingInputGrid" label="Phone Number">
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel><br />
+        <FloatingLabel controlId="floatingInputGrid" label="Email address">
+          <Form.Control type="email" placeholder="name@example.com" />
         </FloatingLabel>
       </Col>
     </Row>
