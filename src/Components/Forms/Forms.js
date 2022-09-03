9a5ba5301b752a7c2ea7bf6ea1b4158801b272a4
Richardjackson-web <richardjackson556@gmail.com>
2022-09-03 06:02:29 +0100

    Still making changes

--- src/Components/Forms/Forms.js
+++ src/Components/Forms/Forms.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './Form.css';
 import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Forms() {
     const [validated, setValidated] = useState(false);
@@ -23,39 +23,49 @@ export default function Forms() {
         <h1 className="register">Register for LPC Conference</h1>
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Row className="mb-3">
+        <Col xs={6}>
         <Form.Group controlId="validationCustom01">
           <Form.Label>First name</Form.Label>
           <Form.Control
+          size="sm"
             required
             type="text"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group><br />
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
+        </Form.Group>
+        </Col>
+        <Col xs={6}>
+        <Form.Group controlId="validationCustom02">
           <Form.Label>Last name</Form.Label>
           <Form.Control
             required
             type="text"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group><br />
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
+        </Form.Group>
+        </Col>
+        <Col xs={6}>
+        <Form.Group controlId="validationCustom02">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             required
             type="text"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group><br />
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
+        </Form.Group>
+        </Col>
+        <Col xs={6}>  
+        <Form.Group controlId="validationCustom02">
           <Form.Label>Phone Number</Form.Label>
           <Form.Control
             required
             type="text"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group><br />
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
+        </Form.Group>
+        </Col>
+        <br />
+        <Form.Group controlId="validationCustom02">
           <Form.Label>Expectations from the conference</Form.Label>
           <Form.Control
             required
